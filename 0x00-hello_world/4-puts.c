961346bff736669c4a5e91fae1d0be3936352deb
hattysmiles <hattysmiles>
2022-06-30 15:16:33 -0700

    my put commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,33 @@
+ /*
+ *
+ * File: 4-puts.c
+ *
+ * Auth: Habibat Adetoro <hattysmiles@gmail.com>
+ *
+ */
+
+
+
 #include <stdio.h>
 
-/**
- * Main - Main
- * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: Always  (0)
+ /**
+ *
+ * main - Prints "Programming is like building a multilingual
+ *
+ *                   puzzle, followed by a new line.
+ *
+ *    
+ *
+ * Return: Always 0.
+ *
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+
+		puts("\"Programming is like building a multilingual puzzle")
+			return (0);
+
+}
+
+
