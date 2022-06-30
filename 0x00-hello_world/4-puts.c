34a8286e31f3e659da744d042ce4d51508859f63
hattysmiles <hattysmiles>
2022-06-30 12:55:42 -0700

    my put
    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
-#include <stdio>
+#include <stdio.h>
+
+
 
 /**
- * main - Entry point
  *
- * Return: Always 0 (success)
- */
-int main (void)
-{
-        puts("\"Programming is like building a multilingual puzzle");
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-	return (0);
-}	
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
