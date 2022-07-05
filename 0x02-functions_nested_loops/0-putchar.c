71a35bc7d24f0933ab771ab17cff8e7107e0abbf
hattysmiles <hattysmiles>
2022-07-05 04:27:00 -0700

    putchar zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the word _putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char word [9] = "_putchar"
+	int i;
+	for(i=0, i <= 8 i++)
+	{
+		putchar(word[i])
+	}
+	putchar('\n')
+	return(0)
+}
