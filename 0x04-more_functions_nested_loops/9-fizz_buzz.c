a411b6ae431870a0c3bc5f1428cdef8d4f0519ff
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-07-08 00:35:04 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 /**
-* main - main function
-* @void: no argument
-* Return: 0 Success
-*/
+ * main - replace multiples of 3 and 5
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
-		if (i % 15 == 0)
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
+			printf("%s", "FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("%s", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
-	printf("Buzz");
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
 	printf("\n");
-
 	return (0);
 }
