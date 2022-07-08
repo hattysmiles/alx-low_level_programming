e2bc9fbaab450a483b321793b39b80d9bd879df7
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-07-08 22:18:06 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,17 +4,15 @@
 * main - Comment causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
-
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	while (i < 10) 
+	while (i < 10);
 	Infinite loop, there is not increment for i
 	{
 	putchar(i);
