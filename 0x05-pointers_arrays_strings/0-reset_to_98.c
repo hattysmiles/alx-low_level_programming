25aeffb6fbea098ce68cfef92cb5bac7f8e26534
hattysmiles <hattysmiles>
2022-07-12 08:35:34 -0700

    98 reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <Stdio>
+/**
+ * main - functions that takes a pointer to int
+ * Return: Always zero
+ */
+int main(void)
+{
+	int n;
+	int *n;
+	n = 56;
+	*n = &n;
+	printf("n=%d\n",n);
+	*n = 98;
+	printf("n=%d\n",n);
+	return (0);
+}	
