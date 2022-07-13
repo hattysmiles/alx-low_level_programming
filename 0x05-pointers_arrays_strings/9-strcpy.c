d3ad88ec536d446c62ea128b6913c6064b88e7a5
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-07-13 02:41:02 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * Strcpy - function that copies string poined to by src
- * @dest: Buffer dest
+ * _strcpy - function that copies the string pointed to by src
+ * @dest: buffer dest
  * @src: string
  * Return: value pointer to dest
  */
 char *_strcpy(char *dest, char *src)
+{
 	int index;
 
 	index = 0;
@@ -13,11 +14,12 @@ char *_strcpy(char *dest, char *src)
 	while (index >= 0)
 	{
 		dest[index] = src[index];
+
 		if (src[index] == '\0')
 		{
 			return (dest);
 		}
 		index++;
 	}
-	return (dest)
+	return (dest);
 }
