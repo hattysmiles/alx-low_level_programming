a30ce11c5c899082800c6c951d19473911199530
hattysmiles <hattysmiles>
2022-07-13 07:46:21 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * puts2 - function prints every other character of a string
- * @str: string
+ * @s: string
  * Return: nothing
  */
-void puts2(char *str)
+void puts2(char *s)
 {
 	int A, a, b;
 	char temp;
@@ -12,7 +12,7 @@ void puts2(char *str)
 	A = 0;
 	while (A >= 0)
 	{
-		if (str[A] == '\0')
+		if (s[A] == '\0')
 			break;
 		A++;
 	}
@@ -20,9 +20,9 @@ void puts2(char *str)
 	{
 		for (b = a + 1; b > 0; b--)
 		{
-			temp = *(str + b);
-			*(str + b) = *(str + (b - 1));
-			*(str + (b - 1)) = temp;
+			temp = *(s + b);
+			*(s + b) = *(s + (b - 1));
+			*(s + (b - 1)) = temp;
 		}
 	}
 }
