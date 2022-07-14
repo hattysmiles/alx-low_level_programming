07b1c548a2f0521652c300aeede7448d487d32c4
hattysmiles <hattysmiles>
2022-07-14 12:12:00 -0700

    string_toupp
    er

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -15,8 +15,7 @@ char *string_toupper(char *str)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = (char)(str[i] - 32);
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] = (char)(str[i] + 32);
+		i++;
 	}
 	return (str);
 }
