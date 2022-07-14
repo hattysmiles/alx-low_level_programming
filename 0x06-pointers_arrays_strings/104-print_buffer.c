5f8166716285ad9a1828d0c4b741e1740fa307ad
hattysmiles <hattysmiles>
2022-07-14 16:44:57 -0700

    infinite_add

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,15 @@
+##include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * isPrintableASCII - determines if n is a printable ASCII char
+ *
+ *   * @n: integer
+ *
+ *    * Return: 1 if true, 0 if false
+ *
+ *     */
