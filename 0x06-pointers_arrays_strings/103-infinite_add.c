1a9f53186a2abe6462b1fdb8e428c2cc91edff8a
hattysmiles <hattysmiles>
2022-07-14 16:33:26 -0700

    infinite_add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *  * infinite_add -  adds two numbers
+ *
+ *   * @n1: first number
+ *
+ *    * @n2: second number
+ *
+ *     * @r: result
+ *
+ *      * @size_r: result lenght
+ *
+ *       * Return: sum
+ *
+ *        *
+ *
+ *         */
