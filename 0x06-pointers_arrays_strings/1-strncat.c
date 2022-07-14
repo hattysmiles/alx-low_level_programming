dcd4bdc96d95553a48b2777d6a71a3a7da491203
hattysmiles <hattysmiles>
2022-07-14 06:14:22 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * it will use at most n bytes from src
+ * @desc: input value
+ * @src: input value
+ * @n: input value
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	j = 0;
+	while ( b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
