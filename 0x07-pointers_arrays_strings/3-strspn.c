01be765124e4180b3640c22f3262515352297cd2
hattysmiles <hattysmiles>
2022-07-18 07:51:48 -0700

    strspn 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - functions that gets the length of a prefix substring
+ * @s: the string
+ * @accept: contains bytes from accept
+ *
+ * Return: the number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a;
+	int j;
+	unsigned int length;
+
+	length = 0;
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0' && accept[b] != s[a]; b++)
+			;
+		if (s[a] == accept[b])
+			length++;
+		if (accept[j] == '\0')
+			return (length);
+	}
+	return (length);
+
+}
