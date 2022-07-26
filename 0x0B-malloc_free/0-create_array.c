3c5f76fde73a5dbf79879419186dca35a6391a0f
hattysmiles <hattysmiles>
2022-07-26 12:03:44 -0700

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,21 +10,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *s;
+	char *array;
 	unsigned int i = 0;
 
-	s = malloc(size * sizeof(char));
+	array = malloc(sizeof(*array) * size);
 
-	if (s == NULL)
+	if (array == NULL || size == 0)
 		return (NULL);
-
-	if (size == 0)
-		return (NULL);
-
-	while (i < size)
-	{
-		s[i] = c;
-		i++;
-	}
-	return (s);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
 }
