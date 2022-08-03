a1f0b75e0ff31958900b3d09fdfe79c98dc787d4
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-08-04 00:04:36 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,55 +1,37 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
+ * int_index - function that searches
+ * for an integer
+:wq
+*
+ * @array: array with parameters
+ * @size: size of the array
+ * @cmp: function to call function
  *
- * Return: 0 if false, something else otherwise.
+ * Return: returns the index of the first element
+ * for which the cmp function does not return 0
  */
-int is_98(int elem)
+int int_index(int *array, int size, int (*cmp)(int))
 {
-    return (98 == elem);
-}
+	int i;
 
-/**
- * is_strictly_positive - check if a number is greater than 0
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_strictly_positive(int elem)
-{
-    return (elem > 0);
-}
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
 
+	if (size <= 0)
+	{
+		return (-1);
+	}
 
-/**
- * abs_is_98 - check if the absolute value of a number is 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int abs_is_98(int elem)
-{
-    return (elem == 98 || -elem == 98);
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-    int index;
-
-    index = int_index(array, 20, is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, abs_is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, is_strictly_positive);
-    printf("%d\n", index);
-    return (0);
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) == 1)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
