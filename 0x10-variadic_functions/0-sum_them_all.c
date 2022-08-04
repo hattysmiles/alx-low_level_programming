107d98516f062caa92d6821e4b7f3ed3e0e2a736
hattysmiles <hattysmiles>
2022-08-04 11:29:32 -0700

    variadic f

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Entry Point
+ * @b: variadic inputs
+ * Return: sum
+ */
+int sum_them_all(const unsigned int b, ...)
+{
+	va_list valist;
+	unsigned int i, sum = 0;
+
+	if (b == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(valist, const unsigned int);
+	}
+	va_end(valist);
+
+	return (sum);
+}
