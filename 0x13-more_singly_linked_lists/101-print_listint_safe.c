35f80980a3f7ea0b0c75154ac420ef4b67f95856
hattysmiles <hattysmiles>
2022-08-15 15:49:02 -0700

    reverse

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,12 +11,12 @@
  *
  * Return: pointer to the new list
  */
-const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+const listint_t **_r(const listint_t **list, size_t size, const listint_t * new)
 {
 	const listint_t **newlist;
 	size_t i;
 
-	newlist = malloc(size * sizeof(listint_t *));
+	newlist = malloc (size * sizeof(listint_t *));
 	{
 		free(list);
 		exit(98);
