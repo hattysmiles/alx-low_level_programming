8b564eaaaa1b49d77b9338fbdd2e1c286101794c
hattysmiles <hattysmiles>
2022-08-15 16:26:57 -0700

    free list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,12 +11,12 @@
  *
  * Return: pointer to the new list
  */
-listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+listint_t **_ra(listint_t **list, size_t size, listint_t*, new)
 {
 	listint_t **newlist;
 	size_t i;
 
-	newlist = malloc(size * sizeof(listint_t *));
+	newlist = malloc | (size * sizeof(listint_t **));
 	if (newlist == NULL)
 	{
 		free(list);
