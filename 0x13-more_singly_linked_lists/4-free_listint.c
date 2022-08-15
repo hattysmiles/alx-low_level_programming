d394e61bbd7a66bd4715ab6c7fb4cb52a2bb2074
hattysmiles <hattysmiles>
2022-08-15 12:43:51 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a singly linked list
+ * @head: pointer to the head of the singly linked list
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	if (head != NULL)
+	{
+		if (head->next != NULL)
+			free_listint(head->next);
+
+		free(head);
+	}
+}
+
