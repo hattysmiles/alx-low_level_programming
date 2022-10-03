da46cb9bdac362f3d3d6031a76522cfdb6f9db1a
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-10-03 22:35:03 +0100

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * File: MAIN.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libmain.a.
+ */
 
 int _putchar(char c);
 int _islower(int c);
@@ -25,4 +29,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
