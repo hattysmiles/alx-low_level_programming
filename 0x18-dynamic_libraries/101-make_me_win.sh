e4e1436bd649c5c4b1889143fbce4d7d79d953d5
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-10-08 02:04:11 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://raw.githubusercontent.com/hattysmiles/alx-low_level_programming/main/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
